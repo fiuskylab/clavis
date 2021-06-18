a5e1bc603f02978f10873a10058bf0099bb4e2c1
Rafael Breno <rafael_breno@outlook.com>
2021-06-18 17:33:44 -0300

    feat: Add internal error handler pkg

---
+++ errors.go
@@ -0,0 +1,53 @@
+package clavis
+
+import "fmt"
+
+const (
+	templateNotFound = "%s not found"
+	templateMissing  = "Missing %s"
+	templateExists   = "%s already exists"
+	templateExpired  = "%s is expired"
+)
+
+type errat struct {
+	message string
+	isNil   bool
+}
+
+func NilErrat() errat {
+	return errat{
+		isNil: true,
+	}
+}
+
+func ErratNotFound(key string) errat {
+	return errat{
+		isNil:   false,
+		message: fmt.Sprintf(templateNotFound, key),
+	}
+}
+
+func ErratMissing(key string) errat {
+	return errat{
+		isNil:   false,
+		message: fmt.Sprintf(templateMissing, key),
+	}
+}
+
+func ErratExists(key string) errat {
+	return errat{
+		isNil:   false,
+		message: fmt.Sprintf(templateExists, key),
+	}
+}
+
+func ErratExpired(key string) errat {
+	return errat{
+		isNil:   false,
+		message: fmt.Sprintf(templateExpired, key),
+	}
+}
+
+func (e *errat) Nil() bool {
+	return e.isNil
+}
